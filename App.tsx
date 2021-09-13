5a749b9f24dc0e5adf7a34977206694a9041e5f8
Ade Fahri Syany <fahrisyany@gmail.com>
2021-09-13 23:36:57 +0700

    initial commit with layout only

--- App.tsx
+++ App.tsx
@@ -1,21 +1,111 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, Layout, Text, Input, Card, Button } from '@ui-kitten/components';
+import { StyleSheet, View } from 'react-native';
 
-export default function App() {
+const useInputState = (initialValue = '') => {
+  const [value, setValue] = React.useState(initialValue);
+  return { value, onChangeText: setValue };
+};
+
+const Header = (props: any) => (
+  <View {...props}>
+    <Text category='h1'>N-32</Text>
+  </View>
+);
+
+export const CardAccessoriesShowcase = () => (
+  <React.Fragment>
+    <Layout style={styles.topContainer} level='1'>
+      <Card style={styles.card} header={Header}>
+        <Text category='h5'>60/40 C</Text>
+      </Card>
+    </Layout>
+  </React.Fragment>
+);
+
+const HomeScreen = () => {
+
+  const successInputState = useInputState();
+  const infoInputState = useInputState();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <Layout style={styles.container}>
+      <Text category='h2' style={styles.title} >SUKOWANGI</Text>
+      <View style={styles.rowContainer}>
+        <Text style={styles.label} category='label'>Kode Petani :</Text>
+        <Input
+          style={styles.input}
+          {...successInputState}
+        />
+      </View>
+      <Layout style={styles.rowContainer}>
+        <Text style={styles.label} category='label'>No Keranjang :</Text>
+        <Input
+          style={styles.input}
+          {...infoInputState}
+        />
+      </Layout>
+      {CardAccessoriesShowcase()}
+      <View style={styles.buttonContainer}>
+        <Button appearance='ghost' status='control'>
+          Cari
+        </Button>
+      </View>
+    </Layout>
+
+  )
+
+};
+
+export default () => (
+  <ApplicationProvider {...eva} theme={eva.light}>
+    <HomeScreen />
+  </ApplicationProvider>
+);
 
 const styles = StyleSheet.create({
+  title: {
+    marginBottom: 32,
+  },
+  label: {
+    marginRight: 16,
+  },
+  input: {
+    flex: 1
+  },
+  rowContainer: {
+    marginBottom: 8,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: "row",
+    // borderWidth: 1,
+    // borderColor: "thistle",
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    padding: 16,
+    paddingTop: 64,
+    flexDirection: "column",
+    // borderWidth: 1,
+    // borderColor: "red",
+  },
+  topContainer: {
+    marginTop: 26,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  card: {
+    flex: 1,
+  },
+  buttonContainer: {
+    marginTop: 16,
+    padding: 6,
+    width: '100%',
+    borderRadius: 4,
     justifyContent: 'center',
+    backgroundColor: '#3366FF',
   },
 });
