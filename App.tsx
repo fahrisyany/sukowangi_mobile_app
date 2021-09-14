9d8e20400e0aceb3b0a274c7ca29df6509e4b6fc
Ade Fahri Syany <fahrisyany@gmail.com>
2021-09-14 20:48:24 +0700

    Created a new Expo app

--- App.tsx
+++ App.tsx
@@ -1,92 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import * as eva from '@eva-design/eva';
-import { ApplicationProvider, Layout, Text, Input, Divider, Button } from '@ui-kitten/components';
-import { StyleSheet, View } from 'react-native';
-import { CardCustom } from './components/CardCustom'
-import * as Device from 'expo-device';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-const useInputState = (initialValue = '') => {
-  const [value, setValue] = React.useState(initialValue);
-  return { value, onChangeText: setValue };
-};
+import useCachedResources from './hooks/useCachedResources';
+import useColorScheme from './hooks/useColorScheme';
+import Navigation from './navigation';
 
+export default function App() {
+  const isLoadingComplete = useCachedResources();
+  const colorScheme = useColorScheme();
 
-const HomeScreen = () => {
-
-  const successInputState = useInputState();
-  const infoInputState = useInputState();
-
-  return (
-    <Layout style={styles.container}>
-      <Text category='h2' style={styles.title} >SUKOWANGI</Text>
-      <Divider />
-      <View style={styles.rowContainer}>
-        <Text style={styles.label} category='label'>Kode Petani {'   '}:</Text>
-        <Input
-          style={styles.input}
-          {...successInputState}
-        />
-      </View>
-      <View style={styles.rowContainer}>
-        <Text style={styles.label} category='label'>No Keranjang :</Text>
-        <Input
-          style={styles.input}
-          keyboardType={Device.osName === "Android" ? "numeric" : "number-pad"}
-          {...infoInputState}
-        />
-      </View>
-      {CardCustom()}
-      <View style={styles.buttonContainer}>
-        <Button appearance='ghost' status='control'>
-          Cari
-        </Button>
-      </View>
-    </Layout>
-  )
-};
-
-export default () => (
-  <ApplicationProvider {...eva} theme={eva.light}>
-    <HomeScreen />
-  </ApplicationProvider>
-);
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'flex-start',
-    padding: 16,
-    paddingTop: 64,
-    flexDirection: "column",
-    backgroundColor: '#f7f8fc'
-    // borderWidth: 1,
-    // borderColor: "red",
-  },
-  title: {
-    marginBottom: 32,
-  },
-  label: {
-    marginRight: 16,
-  },
-  input: {
-    width: 260
-  },
-  rowContainer: {
-    marginBottom: 8,
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    flexDirection: "row",
-    width: '100%',
-    // borderWidth: 1,
-    // borderColor: "thistle",
-  },
-  buttonContainer: {
-    marginTop: 16,
-    padding: 6,
-    width: '100%',
-    borderRadius: 8,
-    justifyContent: 'center',
-    backgroundColor: '#2897fc',
-  },
-});
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <SafeAreaProvider>
+        <Navigation colorScheme={colorScheme} />
+        <StatusBar />
+      </SafeAreaProvider>
+    );
+  }
+}
